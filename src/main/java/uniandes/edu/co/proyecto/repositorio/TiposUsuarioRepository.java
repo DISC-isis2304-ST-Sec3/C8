8786e2e644af85c8fd28c5a3453d3cceb60b151d
Camilo Mercado <j.mercado@uniandes.edu.co>
2023-09-27 23:55:58 -0500

    Repo req 1

---
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/TiposUsuarioRepository.java
@@ -0,0 +1,28 @@
+package uniandes.edu.co.proyecto.repositorio;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import uniandes.edu.co.proyecto.modelo.TiposUsuario;
+
+public interface TiposUsuarioRepository extends JpaRepository<TiposUsuario, Integer> {
+    
+    @Query(value = "SELECT * FROM TiposUsuario", nativeQuery = true )
+    Collection<TiposUsuario> darTiposUsuario();
+    
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO TiposUsuario (tipo) VALUES (:tipo)", nativeQuery = true )
+    void insertarTipoUsuario(@Param("tipo") String tipo);
+    
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM TiposUsuario WHERE tipo = :tipo ", nativeQuery = true )
+    void eliminarTipoUsuario(@Param("tipo") String tipo);
+    
+}
