1a349fec6eee83141f25b88b5ae5d5563d6a2b27
Camilo Mercado <j.mercado@uniandes.edu.co>
2023-09-28 23:34:59 -0500

    AYUDA

--- src/main/java/uniandes/edu/co/proyecto/repositorio/UsuariosRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/UsuariosRepository.java
@@ -10,25 +10,25 @@ import org.springframework.transaction.annotation.Transactional;
 
 import uniandes.edu.co.proyecto.modelo.Usuarios;
 
-public interface UsuariosRepository extends JpaRepository<Usuarios, Integer> {
+public interface UsuariosRepository extends JpaRepository<Usuarios, String> {
     
-    @Query(value = "SELECT * FROM Usuarios", nativeQuery = true )
+    @Query(value = "SELECT * FROM Usuarios ", nativeQuery = true )
     Collection<Usuarios> darUsuarios();
     
-    @Query(value = "SELECT * FROM Usuarios WHERE tipo_documento = :tipoDoc AND num_documento = :numDoc AND nombre = :nombre AND correo = :correo", nativeQuery = true )
+    @Query(value = "SELECT * FROM Usuarios WHERE tipo_documento = :tipoDoc AND num_documento = :numDoc AND nombre = :nombre AND correo = :correo ", nativeQuery = true )
     Usuarios darUsuario(@Param("tipoDoc") String tipoDoc, @Param("numDoc") Long numDoc, @Param("nombre") String nombre, @Param("correo") String correo);
     
-    @Query(value = "SELECT * FROM Usuarios WHERE tipo_documento = :tipoDoc AND num_documento = :numDoc", nativeQuery = true )
+    @Query(value = "SELECT * FROM Usuarios WHERE tipo_documento = :tipoDoc AND num_documento = :numDoc ;", nativeQuery = true )
     Usuarios darUsuarioPorNumDocumento(@Param("tipoDoc") String tipoDoc, @Param("numDoc") Long numDoc);
     
     @Modifying
     @Transactional
-    @Query(value = "INSERT INTO Usuarios (tipo_documento, num_documento, nombre, correo, TiposUsuario_tipo) VALUES (:tipo_documento , :num_documento , :nombre , :correo , :tipo_user)", nativeQuery = true )
+    @Query(value = "INSERT INTO Usuarios (tipo_documento, num_documento, nombre, correo, TiposUsuario_tipo) VALUES (:tipo_documento , :num_documento , :nombre , :correo , :tipo_user) ", nativeQuery = true )
     void insertarUsuario(@Param("tipo_documento") String tipo_documento, @Param("num_documento") Long num_documento,@Param("nombre") String nombre, @Param("correo") String correo, @Param("tipo_user") String TiposUsuario_tipo);
     
     @Modifying
     @Transactional
-    @Query(value = "UPDATE Usuarios SET TiposUsuario_tipo = :tipo_user WHERE tipo_documento = :tipo_documento AND num_documento = :num_documento)", nativeQuery = true )
+    @Query(value = "UPDATE Usuarios SET TiposUsuario_tipo = :tipo_user WHERE tipo_documento = :tipo_documento AND num_documento = :num_documento ) ", nativeQuery = true )
     void actualizarUsuario(@Param("tipo_documento") String tipo_documento, @Param("num_documento") Long num_documento, @Param("tipo_user") String TiposUsuario_tipo);
     
     
